7d4540c5adf5ba4c0f6d88a964c86c824e7c627a
Lau Yee Heng <lauyh87@gmail.com>
2021-10-17 00:17:59 +0800

    Update Navbar.js

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import {Link} from 'react-router-dom'
 
 // class based react component 
 
@@ -35,10 +35,10 @@ const Navbar = ({icon, title}) => {
 			<h1><i className={icon}></i>  {title} </h1>
 			<ul>
 				<li>
-					<a href='/'>Home</a>
+					<Link to='/'>Home</Link> 
 				</li>
 				<li>
-					<a href='/about'>About</a>
+					<Link to='/about'>About</Link>
 				</li>
 			</ul>
 		</nav>
