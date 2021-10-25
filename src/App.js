b030fbfd2ba7f769b018754465c2401ec1ffbe9c
Richard <32272387+lauyh@users.noreply.github.com>
2021-10-26 01:10:07 +0800

    minor code refactor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
 import Navbar from "./components/layouts/Navbar";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
 import Alert from "./components/layouts/Alert";
 import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import "./App.css";
 
 const App = () => {
@@ -24,12 +24,7 @@ const App = () => {
               <Route
                 exact
                 path="/"
-                render={(props) => (
-                  <Fragment>
-                    <Search />
-                    <Users/>
-                  </Fragment>
-                )}
+                component={Home}
               />
               <Route exact path="/about" component={About} />
               <Route
@@ -37,6 +32,7 @@ const App = () => {
                 path="/user/:login"
                 component={User}
               />
+              <Route component={NotFound} />
             </Switch>
           </div>
         </div>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const NotFound = () => {
+    return (
+        <div>
+            <h1>Not Found</h1>
+            <p className="lead">The page that you are looking for is not existed...</p>
+        </div>
+    )
+}
+
+export default NotFound;
